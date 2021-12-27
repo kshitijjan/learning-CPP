01c22cd29ace688a88306a0ba7b5d4485a275fad
Kshitij jain <kshitijjain@Kshitijs-MacBook-Air.local>
2021-12-28 01:20:18 +0530

    Data Types and Variables in c++

---
+++ Data-types-and-Variables/4-data-variables.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  string first_name, last_name;
+  cout<<"Enter your first and last name"<<endl;
+  cin>>first_name>>last_name;
+
+  int age, year;
+  cout<<"Enter your age"<<endl;
+  cin>>age;
+  cout<<"Hello, "<<first_name<<" "<<last_name<<" your age is "<<age<<endl;
+
+
+
+  return 0;
+}
